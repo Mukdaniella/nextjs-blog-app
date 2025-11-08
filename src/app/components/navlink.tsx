5f1c8bccc930893dde11fc682c8c9a51f6c84caf
Mukdaniella <mugabodaniella22@gmail.com>
2025-11-08 21:24:17 +0200

    i worked on the navlink component

---
+++ src/app/components/navlink.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+
+export default function NavLink({ href, children }: { href: string; children: React.ReactNode }) {
+return (
+<Link href={href} className="hover:underline">
+{children}
+</Link>
+);
+}
